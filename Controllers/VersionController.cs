40b539137d6e9bd9ae219b0ebbabc2b37fa30b4d
serhii.horobets <serhii.horobets@itera.no>
2019-02-19 17:35:17 +0200

    Add version controller

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Evoflare.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Evoflare.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        public VersionController()
+        {
+        }
+
+        [HttpGet("", Name = "GetVersion")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Version of application.", typeof(string))]
+        public string Get()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
