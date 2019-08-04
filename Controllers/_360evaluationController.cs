62c0d634633c169abfe07e8e97c1104e5f2bdb90
Oleksii Chornousov <oleksii.chornousov@gmail.com>
2019-08-04 20:56:00 +0300

    Controller for 360 evaluation analytics

--- Controllers/_360evaluationController.cs
+++ Controllers/_360evaluationController.cs
@@ -285,6 +285,313 @@ namespace Evoflare.API.Controllers
             };
         }
 
+        public class Criteria
+        {
+            public int FeedbacksGiven { get; set; }
+            //public Criterias FeedbacksGivenCriteria { get; set; }
+            public double MeanValue { get; set; }
+            //public Criterias MeanValueCriteria { get; set; }
+            //public ICollection<int> NotContainsMarks { get; set; }
+            public double NotContainsMarksTreshold { get; set; }
+            //public Criterias NotContainsMarksTresholdCriteria { get; set; }
+
+            public int CommentLengthWords { get; set; }
+            //public Criterias CommentLengthWordsCriteria { get; set; }
+
+            //public enum Criterias
+            //{
+            //    More,
+            //    MoreOrEqual,
+            //    Less,
+            //    LessOrEqual,
+
+            //    Equal, // +/- 0.2
+
+            //    LessThanMean,
+            //    GreatedThanMean
+            //}
+
+            //public class FeedbackCriteria
+            //{
+            //    public int FeedbacksGiven { get; set; }
+            //    public Criterias Criterias { get; set; }
+            //}
+        }
+        public enum AnalysisGroups
+        {
+            Kind,
+            Righteous,
+            Hardworker
+        }
+        public class Analytics360Settings
+        {
+            public AnalysisGroups Group { get; set; }
+            public Criteria Criteria { get; set; }
+        }
+        private Analytics360Settings[] GetCriterias()
+        {
+            var criterias = new[]
+            {
+                new Analytics360Settings
+                {
+                    Group = AnalysisGroups.Kind,
+                    Criteria = new Criteria
+                    {
+                        FeedbacksGiven = 3,
+                        //FeedbacksGivenCriteria = Criteria.Criterias.More,
+                        MeanValue = 4.1,
+                        //MeanValueCriteria = Criteria.Criterias.More,
+                        //NotContainsMarks = new HashSet<int>(new[] { 1, 2 })
+                    }
+                },
+                new Analytics360Settings
+                {
+                    Group = AnalysisGroups.Righteous,
+                    Criteria= new Criteria
+                    {
+                        FeedbacksGiven = 3,
+                        //FeedbacksGivenCriteria = Criteria.Criterias.More,
+                        MeanValue = 2.7,
+                        //MeanValueCriteria = Criteria.Criterias.Less,
+                        //NotContainsMarksTresholdCriteria = Criteria.Criterias.LessThanMean
+                    }
+                },
+                new Analytics360Settings
+                {
+                    Group = AnalysisGroups.Hardworker,
+                    Criteria = new Criteria
+                    {
+                        //FeedbacksGivenCriteria = Criteria.Criterias.GreatedThanMean,
+                        MeanValue = 3,
+                        //MeanValueCriteria = Criteria.Criterias.Equal,
+                        CommentLengthWords = 3,
+                        //CommentLengthWordsCriteria = Criteria.Criterias.LessOrEqual
+                    }
+                }
+            };
+            return criterias;
+        }
+        //private bool ValidateForCriteria(Criteria criteria, _360employeeEvaluation employeeEvaluation, ICollection<_360employeeEvaluation> allEvaluations)
+        //{
+        //    var checklist = new List<bool>();
+        //    var feedbackCriteria = new Dictionary<Criteria.Criterias, Func<int, int, bool>>
+        //    {
+        //        { Criteria.Criterias.More, (expected, actual) => actual > expected }
+        //    };
+        //    if (feedbackCriteria.TryGetValue(criteria.FeedbacksGivenCriteria, out var simpleCriteria))
+        //    {
+        //        checklist.Add(simpleCriteria(criteria.FeedbacksGiven, employeeEvaluation._360evaluation.Count));
+        //    } 
+        //    else
+        //    {
+        //        if (criteria.FeedbacksGivenCriteria == Criteria.Criterias.GreatedThanMean)
+        //        {
+        //            var meanCount = allEvaluations
+        //                .GroupBy(e => e.EvaluatorEmployeeId)
+        //                .Select(e => e.Count()).Average();
+        //            checklist.Add(employeeEvaluation._360evaluation.Count > meanCount);
+        //        }
+        //    }
+
+        //    var meanFeedbackCriteria = new Dictionary<Criteria.Criterias, Func<double, double, bool>>
+        //    {
+        //        { Criteria.Criterias.More, (expected, actual) => actual > expected },
+        //        { Criteria.Criterias.Less, (expected, actual) => actual > expected },
+        //        { Criteria.Criterias.Equal, (expected, actual) => ((expected + 0.1) > actual) && ((expected - 0.1) < actual) },
+        //    };
+        //    if (meanFeedbackCriteria.TryGetValue(criteria.MeanValueCriteria, out var meanCriteria))
+        //    {
+        //        var mean = employeeEvaluation._360evaluation.Select(e => e.FeedbackMarkId).Average();
+        //        checklist.Add(meanCriteria(criteria.MeanValue, mean));
+        //    }
+
+        //    if (criteria.NotContainsMarks.Any())
+        //    {
+        //        checklist.Add(employeeEvaluation._360evaluation.All(e => !criteria.NotContainsMarks.Contains(e.FeedbackMarkId)));
+        //    }
+        //    if (criteria.NotContainsMarksTresholdCriteria == Criteria.Criterias.LessThanMean)
+        //    {
+        //        var meanMark = allEvaluations
+        //            .SelectMany(e => e._360evaluation)
+        //            .Select(e => e.FeedbackMarkId)
+        //            .Average();
+        //        checklist.Add(employeeEvaluation._360evaluation.All(e => e.FeedbackMarkId < meanMark));
+        //    }
+
+        //    if (criteria.CommentLengthWordsCriteria == Criteria.Criterias.LessOrEqual)
+        //    {
+        //        employeeEvaluation.
+        //    }
+        //    return checklist.All(c => c);
+        //}
+
+        private bool ValidateForKind(
+            ICollection<_360employeeEvaluation> employeeEvaluations, 
+            ICollection<_360employeeEvaluation> allEmployeeEvaluations)
+        {
+            var checks = new List<bool>();
+            var criteria = GetCriterias()[0].Criteria;
+            // дал более 4 оценок
+            checks.Add(employeeEvaluations.Count > criteria.FeedbacksGiven);
+
+            // средний балл выше 4,1
+            var feedbacks = employeeEvaluations.SelectMany(e => e._360evaluation.Select(ev => ev.FeedbackMarkId)).ToList();
+            checks.Add(feedbacks.Average() > criteria.MeanValue);
+
+            // среди оценок отсутствуют единицы и двойки
+            checks.Add(!feedbacks.Contains(1));
+            checks.Add(!feedbacks.Contains(2));
+
+            return checks.All(c => c);
+        }
+        private bool ValidateForRighteous(
+            ICollection<_360employeeEvaluation> employeeEvaluations,
+            ICollection<_360employeeEvaluation> allEmployeeEvaluations)
+        {
+            var checks = new List<bool>();
+            var criteria = GetCriterias()[1].Criteria;
+
+            // сделавших более четырех ревью
+            checks.Add(employeeEvaluations.Count > criteria.FeedbacksGiven);
+
+            // поставивших меньше 2,7
+            var feedbacks = employeeEvaluations.SelectMany(e => e._360evaluation.Select(ev => ev.FeedbackMarkId)).ToList();
+            checks.Add(feedbacks.Average() < criteria.MeanValue);
+
+            // не ставивших оценки выше средней
+            var allFeedbacksMean = allEmployeeEvaluations.SelectMany(e => e._360evaluation.Select(ev => ev.FeedbackMarkId)).Average();
+            checks.Add(feedbacks.All(f => f <= allFeedbacksMean));
+            
+            return checks.All(c => c);
+        }
+        private bool ValidateForHardworker(
+            ICollection<_360employeeEvaluation> employeeEvaluations,
+            ICollection<_360employeeEvaluation> allEmployeeEvaluations)
+        {
+            var checks = new List<bool>();
+            var criteria = GetCriterias()[2].Criteria;
+
+            // провели сравнительно много ревью
+            var meanCount = allEmployeeEvaluations
+                .GroupBy(e => e.EvaluatorEmployeeId)
+                .Select(e => e.Count()).Average();
+            checks.Add(employeeEvaluations.Count > meanCount);
+
+            // средняя оценка — три,
+            var marks = employeeEvaluations
+                .SelectMany(e => e._360evaluation)
+                .Select(e => e.FeedbackMarkId)
+                .ToList();
+            var average = marks.Average();
+            checks.Add(average == criteria.MeanValue);
+            // и дисперсия нулевая
+            checks.Add(Math.Abs(Variance(marks)) < 0.001);
+
+            return checks.All(c => c);
+        }
+        private bool ValidateForSemiHardworker(
+            ICollection<_360employeeEvaluation> employeeEvaluations,
+            ICollection<_360employeeEvaluation> allEmployeeEvaluations)
+        {
+            var checks = new List<bool>();
+            var criteria = GetCriterias()[2].Criteria;
+
+            // провели сравнительно много ревью
+            var meanCount = allEmployeeEvaluations
+                .GroupBy(e => e.EvaluatorEmployeeId)
+                .Select(e => e.Count()).Average();
+            checks.Add(employeeEvaluations.Count > meanCount);
+
+            // средняя оценка — три
+            // !! с небольшим отклонением
+            // !! дисперсию не учитываем
+            var marks = employeeEvaluations
+                .SelectMany(e => e._360evaluation)
+                .Select(e => e.FeedbackMarkId)
+                .ToList();
+            var average = marks.Average();
+            checks.Add(Math.Abs(average - criteria.MeanValue) < 0.1);
+
+            return checks.All(c => c);
+        }
+
+        private double Variance(ICollection<int> nums, double? average = null)
+        {
+            if (nums.Count > 1)
+            {
+                // Get the average of the values
+                double avg = average.HasValue ? average.Value : nums.Average();
+
+                // Now figure out how far each point is from the mean
+                // So we subtract from the number the average
+                // Then raise it to the power of 2
+                double sumOfSquares = 0.0;
+
+                foreach (int num in nums)
+                {
+                    sumOfSquares += Math.Pow((num - avg), 2.0);
+                }
+
+                // Finally divide it by n - 1 (for standard deviation variance)
+                // Or use length without subtracting one ( for population standard deviation variance)
+                return sumOfSquares / (nums.Count - 1);
+            }
+            else { return 0.0; }
+        }
+        
+        // GET: api/_360evaluation/analytics
+        [HttpGet("analytics")]
+        public async Task<ActionResult<dynamic>> Get_360evaluationAnalytics()
+        {
+            // TODO: have filter by employees for manager/hr/other?
+            //var lastEvaluationsCount = 4;
+            var lastEvaluations = await _context.EmployeeEvaluation
+                .Where(r => r.StartDate > DateTime.Now.AddYears(-1))
+                .Include(e => e._360employeeEvaluation)
+                    .ThenInclude(ev => ev._360evaluation)
+                .ToListAsync();
+            var employees = await _context.Employee
+                .Where(e => !e.IsManager)
+                .ToListAsync();
+
+            var rep = new Dictionary<DateTime, Dictionary<int, List<bool>>>();
+            var ids = new HashSet<int>() { 7, 12, 13, 16, 17, 18, 21 };
+            foreach (var p in lastEvaluations.ToLookup(e => e.StartDate))
+            {
+                var dict = new Dictionary<int, List<bool>>();
+                rep.Add(p.Key, dict);
+
+                var period = p;
+                var allEvaluations = period
+                    .SelectMany(c => c._360employeeEvaluation)
+                    .ToList();
+                foreach (var employee in employees.Where(e => ids.Contains(e.Id)))
+                {
+                    var evaluations = period
+                        .SelectMany(c => c._360employeeEvaluation)
+                        .Where(e => e.EvaluatorEmployeeId == employee.Id)
+                        .ToList();
+                    if (evaluations.Any())
+                    {
+                        var c = evaluations.Count;
+                        var isGood = ValidateForKind(evaluations, allEvaluations);
+                        var isBad = ValidateForRighteous(evaluations, allEvaluations);
+                        var isUgly = ValidateForHardworker(evaluations, allEvaluations);
+                        var isSemiUgly = ValidateForSemiHardworker(evaluations, allEvaluations);
+                        var all = new List<bool> { isGood, isBad, isUgly, isSemiUgly };
+                        if (all.Where(_ => _).Count() > 1)
+                        {
+
+                        }
+                        dict.Add(employee.Id, all);
+
+                    }
+                }
+            }
+
+            return rep;
+        }
+
         private bool _360evaluationExists(int id)
         {
             return _context._360evaluation.Any(e => e.Id == id);
